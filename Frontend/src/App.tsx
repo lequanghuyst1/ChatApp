acf9fb689162694975145bd1d858993fa0128114
Lê Quang Huy <lequanghuyst1@gmail.com>
2025-08-29 18:00:35 +0700

    add redux toolkit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,14 +2,18 @@
 import AuthProvider from "./stores/auth";
 import Router from "./routes/sections";
 import ThemeProvider from "./theme";
+import { Provider } from "react-redux";
+import { store } from "./stores/store";
 
 function App() {
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <Router />
-      </ThemeProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </AuthProvider>
+    </Provider>
   );
 }
 
